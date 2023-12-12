f68f11a54c03f7bba24f58c81c357ba7cba96d81
ShiDu <1364136725@qq.com>
2023-12-12 21:57:43 +0800

    solve 0008 23/12/12

---
+++ Array/0018.go
@@ -0,0 +1,34 @@
+package array
+
+import (
+	"bufio"
+	"os"
+	//"fmt"
+	"strings"
+)
+
+/*
+题目：
+思路：
+时间：2023/12/12
+链接：
+*/
+
+func AcmMode0018() {
+	// 多行输入
+	input := bufio.NewScanner(os.Stdin)
+	output := bufio.NewWriter(os.Stdout)
+	// 测试用例过长的时候，要先指定bufio的bufer，否则会读取失败
+	// bufer := make([]byte,2000*1024)
+	// input.Buffer(bufer,len(bufer))
+	for input.Scan() { // 每调用一次Scan,从输入流中读取一行
+		// 获取这一行的输入
+		str := strings.Split(input.Text(), " ")
+		//TODO
+		// 将结果写入缓冲输出区
+		output.WriteString(str[0])
+		//TODO
+	}
+	// 输出答案
+	output.Flush()
+}
